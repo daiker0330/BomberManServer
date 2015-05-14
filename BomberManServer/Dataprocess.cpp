40fcabbbb03e2c48853584e5f420228d14f4c101
DarkForte <laserae86@126.com>
2015-05-14 13:53:07 +0800

    真·修复了退出时没有正确置位的问题

--- BomberManServer/Dataprocess.cpp
+++ BomberManServer/Dataprocess.cpp
@@ -356,7 +356,7 @@ CMessage Dataprocess::Game( CMessage* recv_msg )
 	else if(recv_msg->type2 == MSG_GAME_QUIT)
 	{
 		game_host[now_roomnum].SetAvailable(now_playernum, false);
-		onlineData.ready[now_roomnum][now_playernum] = false;
+		onlineData.ready[now_roomnum][now_playernum-1] = false;
 	}
 	else if(recv_msg->type2 == MSG_GAME_START)
 	{
